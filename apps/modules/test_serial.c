02fa9fdc1ebbfa8ed1d4962c80a01c1625cf25ee
xiaofei <alibaba22001@163.com>
2020-04-03 12:44:55 +0800

    update

--- apps/modules/test_serial.c
+++ apps/modules/test_serial.c
@@ -6,7 +6,7 @@
  *   文件名称：test_serial.c
  *   创 建 者：肖飞
  *   创建日期：2019年10月28日 星期一 10时54分01秒
- *   修改日期：2020年04月03日 星期五 09时37分23秒
+ *   修改日期：2020年04月03日 星期五 12时37分28秒
  *   描    述：
  *
  *================================================================*/
@@ -20,7 +20,7 @@
 #define UDP_LOG
 #include "task_probe_tool.h"
 
-void task_uart_tx(void const *argument)
+static void task_uart_tx(void const *argument)
 {
 	int ret = 0;
 	uart_info_t *uart_info = (uart_info_t *)argument;
@@ -44,7 +44,7 @@ void task_uart_tx(void const *argument)
 
 
 static char rx_msg[64];
-void task_uart_rx(void const *argument)
+static void task_uart_rx(void const *argument)
 {
 	int ret = 0;
 	uart_info_t *uart_info = (uart_info_t *)argument;
@@ -60,6 +60,21 @@ void task_uart_rx(void const *argument)
 			udp_log_hexdump("rx_msg", (const char *)rx_msg, ret);
 		}
 	}
+}
+
+void serial_self_test(UART_HandleTypeDef *huart)
+{
+	uart_info_t *uart_info = alloc_uart_info(huart);
+
+	if(uart_info == NULL) {
+		app_panic();
+	}
+
+	osThreadDef(task_uart_tx, task_uart_tx, osPriorityNormal, 0, 128 * 3);
+	osThreadCreate(osThread(task_uart_tx), uart_info);
+
+	osThreadDef(task_uart_rx, task_uart_rx, osPriorityNormal, 0, 128 * 3);
+	osThreadCreate(osThread(task_uart_rx), uart_info);
 }
 
 static char buffer[128];
--- apps/modules/test_serial.h
+++ apps/modules/test_serial.h
@@ -6,7 +6,7 @@
  *   文件名称：test_serial.h
  *   创 建 者：肖飞
  *   创建日期：2019年10月28日 星期一 10时54分05秒
- *   修改日期：2020年04月03日 星期五 08时55分35秒
+ *   修改日期：2020年04月03日 星期五 12时40分39秒
  *   描    述：
  *
  *================================================================*/
@@ -17,10 +17,12 @@ extern "C"
 {
 #endif
 
+#include "cmsis_os.h"
+#include "app_platform.h"
+
 #ifdef __cplusplus
 }
 #endif
+void serial_self_test(UART_HandleTypeDef *huart);
 void task_test_serial(void const *argument);
-void task_uart_tx(void const *argument);
-void task_uart_rx(void const *argument);
 #endif //_TEST_SERIAL_H
