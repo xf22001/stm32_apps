ce0ae80d86abbfc1c222ba186e0044ab540d9f6b
xiaofei <alibaba22001@163.com>
2020-09-09 15:06:19 +0800

    update

---
+++ apps/modules/app/global.h
@@ -0,0 +1,49 @@
+
+
+/*================================================================
+ *   
+ *   
+ *   文件名称：global.h
+ *   创 建 者：肖飞
+ *   创建日期：2020年09月09日 星期三 09时06分39秒
+ *   修改日期：2020年09月09日 星期三 14时24分56秒
+ *   描    述：
+ *
+ *================================================================*/
+#ifndef _GLOBAL_H
+#define _GLOBAL_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "cmsis_os.h"
+#include "app_platform.h"
+#include "os_utils.h"
+#include "list_utils.h"
+
+#include "global_cls.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+typedef struct {
+	struct list_head list;
+	struct list_head key_list;
+	uint32_t cls;
+} global_cls_t;
+
+typedef struct {
+	struct list_head list;
+	uint32_t key;
+	void *value;
+} global_key_t;
+
+void global_init(void);
+void *global_get_class_key_value(uint32_t cls, uint32_t key);
+int global_set_class_key_value(uint32_t cls, uint32_t key, void *value, uint8_t replace);
+int global_delete_class_key(uint32_t cls, uint32_t key);
+void global_dump_class_key_value(void);
+
+#endif //_GLOBAL_H
