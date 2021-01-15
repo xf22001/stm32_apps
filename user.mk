4744c750370c608ccd934ad7afcddef77045acae
xiaofei <alibaba22001@163.com>
2021-01-15 10:28:11 +0800

    update

--- user.mk
+++ user.mk
@@ -25,8 +25,39 @@ USER_C_SOURCES += apps/modules/os/os_utils.c
 C_SOURCES += $(USER_C_SOURCES)
 
 CFLAGS += $(USER_CFLAGS)
+LDFLAGS += -u _printf_float
+
+IAP_FILE := apps/modules/os/iap.h
+
+define update-iap-include
+	if [ -f $(IAP_FILE) ]; then
+		touch $(IAP_FILE);
+	fi
+endef
+
+ifeq ("$(origin APP)", "command line")
+build-type := .app.stamps
+build-type-invalid := .bootloader.stamps
+CFLAGS += -DUSER_APP
+LDSCRIPT = STM32F207VETx_FLASH_APP.ld
+#$(info $(shell $(update-iap-include)))
+$(info "build app!")
+else
+build-type := .bootloader.stamps
+build-type-invalid := .app.stamps
+LDSCRIPT = STM32F207VETx_FLASH.ld
+#$(info $(shell $(update-iap-include)))
+$(info "build bootloader!")
+endif
 
 default: all
+
+all : $(build-type)
+
+$(build-type) :
+	-rm $(build-type-invalid)
+	$(shell $(update-iap-include))
+	touch $@
 
 cscope: all
 	rm cscope e_cs -rf
@@ -35,8 +66,8 @@ cscope: all
 	$(silent)touch dep_files;
 	$(silent)for f in $$(find . -type f -name "*.d" 2>/dev/null); do \
 		for i in $$(cat "$$f" | sed 's/^.*://g' | sed 's/[\\ ]/\n/g' | sort -h | uniq); do \
-			if test "${i:0:1}" = "/";then \
-				echo "$i" >> dep_files; \
+			if test "$${i:0:1}" = "/";then \
+				echo "$$i" >> dep_files; \
 			else \
 				readlink -f "$$i" >> dep_files; \
 			fi; \
