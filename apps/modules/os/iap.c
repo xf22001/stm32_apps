9fffc32da130fd13f2c34d6aa0801c809e6061d2
xiaofei <alibaba22001@163.com>
2021-05-08 17:10:40 +0800

    update

--- apps/modules/os/iap.c
+++ apps/modules/os/iap.c
@@ -6,7 +6,7 @@
  *   文件名称：iap.c
  *   创 建 者：肖飞
  *   创建日期：2019年12月04日 星期三 10时07分01秒
- *   修改日期：2019年12月04日 星期三 10时10分30秒
+ *   修改日期：2021年05月08日 星期六 17时06分34秒
  *   描    述：
  *
  *================================================================*/
@@ -23,12 +23,11 @@ void jump_to_app(void)
 	if(*iap_en == 1) {
 		if (((*(__IO uint32_t *)USER_FLASH_FIRST_PAGE_ADDRESS) & 0x2FFE0000 ) == 0x20020000) {
 			/* Jump to user application */
-			entry = (app_entry_t)(*(__IO uint32_t *) (USER_FLASH_FIRST_PAGE_ADDRESS + 4));
+			entry = (app_entry_t)(*(__IO uint32_t *)(USER_FLASH_FIRST_PAGE_ADDRESS + 4));
 
 			/* Initialize user application's Stack Pointer */
-			__set_MSP(*(__IO uint32_t *) USER_FLASH_FIRST_PAGE_ADDRESS);
+			__set_MSP(*(__IO uint32_t *)USER_FLASH_FIRST_PAGE_ADDRESS);
 			entry();
 		}
-
 	}
 }
